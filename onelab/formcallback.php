80c6bcd9e9038343bc5e2e789ab665b20871b731
Paul Durbin <pauldurbin69@gmail.com>
2016-05-11 22:45:56 +0100

    debug

--- onelab/formcallback.php
+++ onelab/formcallback.php
@@ -20,12 +20,16 @@ $start = "start.php";
 ini_set('display_errors', 'On');
 
 
-$id_token = $state = "";
+$id_token = $access_token = $state = "";
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $id_token = test_input($_POST["id_token"]);
+  $access_token = test_input($_POST["access_token"]);
   $state = test_input($_POST["state"]);
+  
+  var_dump($_POST);
+  die("");
 }
 
 
@@ -48,30 +52,27 @@ if(isset($state))
 
 session_destroy();
 
-
-
 if($id_token != "")
 {
   try
   {
 	error_log("formcallback.php: " . $id_token);
 	include_once 'JWT.php';
-  
-	//$key_public = openssl_get_publickey(file_get_contents('test.cer'));
-	
 	
 	// validate token
 	$key_public = openssl_get_publickey(file_get_contents('auth_server_public_key.cer'));
 	$jwt = JWT::decode($id_token, $key_public ,true);
-      
-	//var_dump($jwt);
-			
-	//setcookie( "id_token", $id_token, $expiry , '/', '.onelab.citrix.com', true, true);
-	
 
 	session_start();
 	
 	$_SESSION['id_token']=$id_token;
+	
+	if($access_token != "")
+	{
+		$ac_jwt = JWT::decode($access_token, $key_public ,true);
+		
+		$_SESSION['access_token']=$access_token;
+	}
     
 	// return start.php to call
 	header('Location: ' . $start,true,302);
