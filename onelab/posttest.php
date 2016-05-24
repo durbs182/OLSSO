d100c55590a6f8a351767004ad7abce01cf88743
Paul Durbin <pauldurbin69@gmail.com>
2016-05-24 15:39:55 +0100

    add post test

---
+++ onelab/posttest.php
@@ -0,0 +1,38 @@
+
+<?php
+include_once 'JWT.php';
+
+function validatetoken($token) 
+{
+		
+	try
+	{
+		$key_public = openssl_get_publickey(file_get_contents('auth_server_public_key.cer'));
+		
+		$jwt = JWT::decode($token,$key_public ,true);
+		
+		return $jwt;
+	}
+	catch(Exception $e)
+	{
+		$msg = $e->getMessage();
+		echo 'Token validation error: ', $msg , "\n";
+		error_log("validatetoken: invalid token : " . $msg);
+		
+		throw new Exception("validatetoken: invalid token : " . $msg);
+	}
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $access_token = test_input($_POST["access_token"]);
+  
+  $jwt = validatetoken($access_token);
+  
+  echo"employeeNumber: $jwt->employeeNumber";
+}
+
+
+
+
+?>
