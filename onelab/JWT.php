5e92f15d40fc0077d59678076442dabba9495efe
durbs182 <pauldurbin69@gmail.com>
2026-03-17 08:07:02 +0000

    Fix method name from decode to decod

--- onelab/JWT.php
+++ onelab/JWT.php
@@ -36,7 +36,7 @@ class JWT
      * @uses jsonDecode
      * @uses urlsafeB64Decode
      */
-    public static function decode($jwt, $key = null, $verify = true)
+    public static function decod($jwt, $key = null, $verify = true)
     {
         $tks = explode('.', $jwt);
         if (count($tks) != 3) {
